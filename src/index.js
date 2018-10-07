9679cd3b850f5066ca65334923321997042a5f8d
Valentin Webdev <valentine.webdev@gmail.com>
2018-10-07 21:06:28 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+    
+    let N = 1;
+    let k = 0;
+	let res=0;
+
+	for(pair of pairs)
+	{
+        N*=Math.pow(pair[0],pair[1]);
+	}
+	
+	if( N > 100000000) return 0;
+	
+	while(k < N)
+	{
+        let found = true;
+		for(let j = 0;j<s.length;++j)
+		{
+			let kjSum = k+j;
+			if	(	
+					(getNOD(kjSum,N) === 1 && s[j]==='1') || 
+					(getNOD(kjSum,N)!==1 && s[j]==='0')
+				)
+			{
+                continue;
+            }
+			else
+			{
+                found = false;
+                break;
+            }
+        }
+        if( found ) res++;
+        k++;
+    }
+    return res;
+}
+function getNOD(a,b)
+{
+	while(b)
+	{
+		let c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
 }
